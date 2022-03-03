7a78438602d6c2d92e8ce52694a4dd71231211c8
L <l-acs@vivaldi.net>
2022-03-03 17:29:32 -0500

    Remove / refactor redundancies

--- streamlit.py
+++ streamlit.py
@@ -29,9 +29,9 @@ def set_fonts_if_empty (cfg = st.session_state, loc = 'fonts/'):
     return cfg
 
 
-def set_tree_container_if_not_exists ():
-    if 'tree_container' not in st.session_state:
-        st.session_state['tree_container'] = st.empty()
+def set_tree_container_if_not_exists (cfg = st.session_state):
+    if 'tree_container' not in cfg:
+        cfg['tree_container'] = st.empty()
 
 
 def parse (cfg = st.session_state):
@@ -57,11 +57,6 @@ def gen_tree_image (cfg = st.session_state, f = None):
 def reload_tree (cfg = st.session_state, default = False):
     cfg['tree_container'].image(cfg['img'])
 
-def initial_draw (cfg = st.session_state, default = False):
-    if 'tree_container' in cfg and 'img' in cfg:
-        reload_tree(cfg, default)
-
-
 def redraw_tree_if_requested (cfg = st.session_state, default = False):
     if 'reload_tree?' in cfg and cfg['reload_tree?']:
 
@@ -73,7 +68,7 @@ def redraw_tree_if_requested (cfg = st.session_state, default = False):
         gen_tree_image(cfg, f = cfg['default_file'] if default else cfg['output_file'])
         set_tree_container_if_not_exists()
         reload_tree(cfg, default)
-        st.session_state['reload_tree?'] = False
+        cfg['reload_tree?'] = False
 
 
 def inform_of_reload (field, widget_field, cfg = st.session_state): # callback for widget changes
@@ -88,26 +83,24 @@ def swap_and_inform_of_reload (cfield1, cfield2, cfg = st.session_state):
 
 def slidewrap(cfield, label, minv, maxv, step = 5, format = '%i pixels', cfg = st.session_state):
     slider_name = cfield + '_slider'
-    prev = cfg[cfield]
-    out = st.slider(label = label,
-                    value = prev,
-                    min_value = minv,
-                    max_value = maxv,
-                    step = step,
-                    format = format,
-                    key = slider_name, # this will make it accessible in state
-                    on_change = inform_of_reload,
-                    args = (cfield, slider_name, cfg)
+    st.slider(label = label,
+              value = cfg[cfield], # previous value
+              min_value = minv,
+              max_value = maxv,
+              step = step,
+              format = format,
+              key = slider_name, # this will make it accessible in state
+              on_change = inform_of_reload,
+              args = (cfield, slider_name, cfg)
     )
 
 def colorwrap(cfield, label, cfg = st.session_state):
     picker_name = cfield + '_picker'
-    prev = cfg[cfield]
-    out = st.color_picker(label = label,
-                          value = prev,
-                          key = picker_name, # this will make it accessible in state
-                          on_change = inform_of_reload,
-                          args = (cfield, picker_name, cfg)
+    st.color_picker(label = label,
+                    value = cfg[cfield], # previous value
+                    key = picker_name, # this will make it accessible in state
+                    on_change = inform_of_reload,
+                    args = (cfield, picker_name, cfg)
     )
 
 
@@ -121,20 +114,23 @@ def buttonswap(cfield1, cfield2, label, cfg = st.session_state):
     )
 
 def colorwrap_cols (fg_field = 'fg_color', bg_field = 'bg_color', cfg = st.session_state):
-    new_selection = False
+    # new_selection = False
     swap_col, fg_col, bg_col = st.columns([10, 1, 4])
 
     with fg_col:
-        new_selection = new_selection or colorwrap(fg_field, '', cfg)
+        # new_selection = new_selection or
+        colorwrap(fg_field, '', cfg)
 
     with bg_col:
-        new_selection = new_selection or colorwrap(bg_field, '', cfg)
+        # new_selection = new_selection or
+        colorwrap(bg_field, '', cfg)
 
     with swap_col:
         st.caption("Select colors")
-        new_selection = new_selection or buttonswap(fg_field, bg_field, 'Swap tree colors', cfg)
+        # new_selection = new_selection or
+        buttonswap(fg_field, bg_field, 'Swap tree colors', cfg)
 
-    return new_selection
+    # return new_selection
 
 
 def dropdownwrap(cfield, label, options, cfg = st.session_state):
@@ -155,20 +151,18 @@ def dropdownwrap(cfield, label, options, cfg = st.session_state):
 
 def show_configurations (cfg = st.session_state):
     with st.expander("Show advanced options"):
-        l = [
-            slidewrap('W', 'Full image width', 350, 3500),
-            slidewrap('H', 'Full image height', 350, 3500),
+        slidewrap('W', 'Full image width', 350, 3500)
+        slidewrap('H', 'Full image height', 350, 3500)
 
-            slidewrap('font_size', 'Font size', 12, 44, step = 1, format = '%i pt'),
+        slidewrap('font_size', 'Font size', 12, 44, step = 1, format = '%i pt')
+        dropdownwrap('font_style', 'Text font', cfg['fonts_avail'])
 
-            dropdownwrap('font_style', 'Text font', cfg['fonts_avail']),
+        slidewrap('bottom_padding', 'Padding above nodes (after branched to)', 4, 40, step = 2)
+        slidewrap('top_padding', 'Padding below nodes', 4, 40, step = 2)
 
-            slidewrap('bottom_padding', 'Padding above nodes (after branched to)', 4, 40, step = 2),
-            slidewrap('top_padding', 'Padding below nodes', 4, 40, step = 2),
-            colorwrap_cols('fg_color', 'bg_color', cfg),
+        colorwrap_cols('fg_color', 'bg_color', cfg)
+        slidewrap('margin', 'Margins around the tree', 0, 125)
 
-            slidewrap('margin', 'Margins around the tree', 0, 125)
-        ]
 
 
 def textbox (old, cfg = st.session_state):
@@ -203,20 +197,20 @@ def header (name, subtitle, author = '🥷', git_url = 'github.com'):
     st.header(f'{name}  —  {subtitle}')
 
 
-def homepage ():
+def homepage (cfg = st.session_state):
     set_defaults_if_empty()
     set_fonts_if_empty()
 
-    previous_text = st.session_state['sentence']
-    new_text = textbox(previous_text, st.session_state)
+    previous_text = cfg['sentence']
+    new_text = textbox(previous_text, cfg)
 
-    if st.session_state['sentence']: # is this useful?
-        st.session_state['reload_tree?'] = True
+    if cfg['sentence']: # for now, this is needed to (a) generate the tree on first run and (b) redraw on font change
+        cfg['reload_tree?'] = True
 
     show_configurations()
 
     if new_text != previous_text:
-        st.session_state['reload_tree?'] = True
+        cfg['reload_tree?'] = True
         st.experimental_rerun()
 
     redraw_tree_if_requested()
